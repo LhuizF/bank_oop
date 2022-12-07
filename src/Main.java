9ab34f75bbd06d9b98a1067343e73dfe2017337f
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-12-06 22:40:10 -0300

    feat: criando e exibindo funcionarios e seus cargos

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@ import models.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Cargo manager = new Cargo("Gerente");
+        Cargo attendant = new Cargo("Atendente");
+
+        Funcionario fManager = new Funcionario("Natalia", "13324468880", "11940028922", 5000.00, manager);
+        Funcionario fAttendant = new Funcionario("Diego", "05296374122", "22987561354", 2000.00, attendant);
+
+        System.out.print(fManager.toString());
+        System.out.println("\n");
+        System.out.print(fAttendant.toString());
+
         System.out.println("Pq vc é assim Java?");
     }
 }
---
+++ src/models/Funcionario.java
@@ -0,0 +1,49 @@
+package models;
+import interfaces.IUsuario;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Funcionario extends Pessoa implements IUsuario {
+    private Double salario;
+    private ArrayList<Cargo> cargos = new ArrayList<Cargo>();
+
+    public Funcionario(String nome, String cpf, String telefone, Double salario, Cargo cargo){
+        this.setNome(nome);
+
+        if(cpf.length() != 11){
+            throw new Error("CPF inválido");
+        }
+
+        this.setCpf(cpf);
+        this.setTelefone(telefone);
+        this.salario = salario;
+        this.cargos.add(cargo);
+    }
+
+    @Override
+    public Boolean autenticar() {
+        return true;
+    }
+
+    public void addCargo(Cargo cargo){
+        this.cargos.add(cargo);
+    }
+
+    public String toString(){
+        String dadosPessoa = this.getDados();
+        String cargos = "\nCargo(s):";
+        String space = " ";
+
+        for(int i = 0; i < this.cargos.size(); i++){
+            Cargo c = this.cargos.get(i);
+
+            if(i > 0){
+                space = ", ";
+            }
+            
+            cargos = cargos + space + c.getNome();
+        }
+
+        return dadosPessoa + "\nSalario: " + this.salario + cargos;
+    }
+}
