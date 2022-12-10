71958db98b4671049d09d31243d1697e368488cd
Luiz Henrique Ferreira <luizhff@gmail.com>
2022-12-10 11:00:17 -0300

    refactor: rename Main file

--- src/Main1.java
+++ src/Main1.java
@@ -1,12 +1,14 @@
 import models.*;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         Cargo manager = new Cargo("Gerente");
         Cargo attendant = new Cargo("Atendente");
 
         Funcionario fManager = new Funcionario("Natalia", "13324468880", "11940028922", 5000.00, manager);
+        manager.addFuncionario(fManager);
         Funcionario fAttendant = new Funcionario("Diego", "05296374122", "22987561354", 2000.00, attendant);
+        attendant.addFuncionario(fAttendant);
 
         System.out.print(fManager.toString());
         System.out.println("\n");
